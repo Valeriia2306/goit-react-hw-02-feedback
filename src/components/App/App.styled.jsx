997b25c7c0b76279db45653c4c18041d7843027f
Valeriia Frolova <valeriiats1989@gmail.com>
2023-02-26 23:52:09 +0200

    add files

---
+++ src/components/App/App.styled.jsx
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+const Container = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 30px;
+  width: 500px;
+  padding-top: 40px;
+  padding-bottom: 40px;
+  background-color: blue;
+  border-radius: 5px;
+`;
+export { Container };
