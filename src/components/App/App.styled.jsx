47e13b0520c0ab7383d3b15d4dc58b0c81fed6b0
Valeriia Frolova <valeriiats1989@gmail.com>
2023-02-27 00:23:52 +0200

    fix4

--- src/components/App/App.styled.jsx
+++ src/components/App/App.styled.jsx
@@ -3,10 +3,11 @@ import styled from '@emotion/styled';
 const Container = styled.div`
   margin-left: auto;
   margin-right: auto;
-  margin-top: 30px;
+  margin-top: 20px;
+  margin-bottom: 20px;
   width: 500px;
-  padding-top: 40px;
-  padding-bottom: 40px;
+  padding-top: 20px;
+  padding-bottom: 20px;
   background-color: blue;
   border-radius: 4px;
 `;
